acddf293c281b1b4bf08e4fd4db2cf533a9b614d
Spiritbe4r <66176445+Spiritbe4r@users.noreply.github.com>
2021-05-31 10:19:29 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -181,13 +181,7 @@ EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD =os.environ.get('EMAIL_HOST_PASSWORD')
-'''
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'mail.in-planet.net'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'wdavila@in-planet.net'
-EMAIL_HOST_PASSWORD = 'inplaneT.2021'
-EMAIL_BACKEND = 'core.libs.smtp.EmailBackend' '''
+
 
 
 
